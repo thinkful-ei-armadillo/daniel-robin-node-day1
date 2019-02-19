5849499b3366317b4d3b1feff2ff99f3808f6959
Daniel Ors <do7811@gmail.com>
2019-02-19 10:20:24 -0500

    complete starting point files with dependencies

--- app.js
+++ app.js
@@ -4,3 +4,13 @@ const express = require('express');
 const morgan = require('morgan');
 const app = express();
 app.use(morgan('dev'));
+
+// starting syntax for each GET path
+app.get('/', (req, res) => {
+  res.send('Hello Express!');
+});
+
+// setting app to listen on correct port
+app.listen(8000, () => {
+  console.log('Express server is listening on port 8000!');
+});
